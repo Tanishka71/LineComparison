7944b38e3f268d5d47c0cf3819492ff0f52c38d2
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-23 16:54:38 +0530

    calculation of cartesian length

--- LineGeometry.java
+++ LineGeometry.java
@@ -1,5 +1,19 @@
 package LineGeometry;
+
+import java.util.Random;
+
 public class LineGeometry {
+	
+	/*
+	 * @params:x1,x2,y1,y2
+	 * @return:length
+	 * @desc:calculates the cartesian length
+	 */
+	
+	static double cartesianLength(int x1, int y1,int x2,int y2) {
+		double length=Math.sqrt((Math.pow((x2-x1),2)+Math.pow((y2- y1),2)));
+		return length;
+	}
 	/*
 	 * @params:none
 	 * @return:none
@@ -7,6 +21,13 @@ public class LineGeometry {
 	 */
 	public static void main(String []args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
+		Random random=new Random();
+		int x1=random.nextInt(100);
+		int y1=random.nextInt(100);
+		int x2=random.nextInt(100);
+		int y2=random.nextInt(100);
+		double length=cartesianLength(x1,y1,x2,y2);
+		System.out.println("The cartesian length is:"+length);
 	}
 }
 
