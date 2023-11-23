b4dcc470e4a4f80abc74e1ea9f5907a65d38e05f
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-23 16:43:33 +0530

    First initial commit to master branch

---
+++ LineGeometry.java
@@ -0,0 +1,12 @@
+package LineGeometry;
+public class LineGeometry {
+	/*
+	 * @params:none
+	 * @return:none
+	 * @desc:main function prints the message
+	 */
+	public static void main(String []args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+}
+
