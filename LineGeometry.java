ce3c84a4a2274bc14fd250fc778da3ea7f31e8d2
Tanishka Jain <tanishkajain1508@gmail.com>
2023-11-23 17:12:50 +0530

    UC_2_Compare_the_endpoints_of_lines_check_equality

--- LineGeometry.java
+++ LineGeometry.java
@@ -4,30 +4,46 @@ import java.util.Random;
 
 public class LineGeometry {
 	
-	/*
-	 * @params:x1,x2,y1,y2
-	 * @return:length
-	 * @desc:calculates the cartesian length
-	 */
 	
-	static double cartesianLength(int x1, int y1,int x2,int y2) {
-		double length=Math.sqrt((Math.pow((x2-x1),2)+Math.pow((y2- y1),2)));
-		return length;
-	}
 	/*
 	 * @params:none
-	 * @return:none
-	 * @desc:main function prints the message
+	 * @return:double array
+	 * @desc:set the endpoint array
 	 */
-	public static void main(String []args) {
-		System.out.println("Welcome to Line Comparison Computation Program");
+    private static double[] setEndpoints() {
 		Random random=new Random();
 		int x1=random.nextInt(100);
 		int y1=random.nextInt(100);
 		int x2=random.nextInt(100);
 		int y2=random.nextInt(100);
-		double length=cartesianLength(x1,y1,x2,y2);
-		System.out.println("The cartesian length is:"+length);
+        return new double[]{x1, y1, x2, y2};
+    }
+    
+    
+	/*
+	 * @params:double array of line1 & line 2
+	 * @return:boolean true or false
+	 * @desc:checks the condition
+	 */
+    private static boolean isEqualTo(double[] line1, double[] line2) {
+        return line1[0] == line2[0] && line1[1] == line2[1] &&
+               line1[2] == line2[2] && line1[3] == line2[3];
+    }
+
+    
+    
+	/*
+	 * @params:none
+	 * @return:none
+	 * @desc:main function prints the answer 
+	 */
+	public static void main(String []args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+        double[] line1 = setEndpoints();
+        double[] line2 = setEndpoints();
+        boolean areEqual = isEqualTo(line1, line2);
+        System.out.println("are lines are equal: " + areEqual);
+
 	}
 }
 
